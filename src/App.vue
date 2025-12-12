3acd9b3af781014bed50655a2cff1392cb01f1c2
Your Name <chasov90@gmail.com>
2025-12-12 15:44:42 +0200

    Initial Vue project setup

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import IButton from './components/IButton/IButton.vue'
+</script>
+
+<template>
+  <h1>WTF!</h1>
+  <IButton />
+</template>
+
+<style scoped></style>
---
+++ src/components/IButton/IButton.vue
@@ -0,0 +1,3 @@
+<template>
+  <button class="i-button">Click me</button>
+</template>
