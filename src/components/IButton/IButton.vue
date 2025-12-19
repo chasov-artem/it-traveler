89fd557c601e86e892afd72bd8ace1459631c731
Your Name <chasov90@gmail.com>
2025-12-19 13:05:58 +0200

    FavoritePlace

--- src/components/IButton/IButton.vue
+++ src/components/IButton/IButton.vue
@@ -2,6 +2,6 @@
   <button
     class="font-bold text-[16px] leading-[138%] tracking-[-0.03em] text-center text-white rounded-[12px] py-[14px] px-10 w-[196px] h-[56px] bg-[#ffa279]"
   >
-    Почати роботу
+    <slot></slot>
   </button>
 </template>
